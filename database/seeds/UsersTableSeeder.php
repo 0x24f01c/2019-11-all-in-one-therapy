aa3c152c00f291284c40fe1d00c5287633154f35
FS <t3y>
2019-11-18 12:10:29 +0100

    create and seed users table, unused migrations removed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+        public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+    }
+}

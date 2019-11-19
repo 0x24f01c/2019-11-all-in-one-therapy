2688e3b87d611ab0ed83660c29a6ea9280e4cb38
FS <t3y>
2019-11-19 09:05:06 +0100

    added error massage

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,9 @@
     {{-- Login-Formular --}}
     <form method="post" action="{{  route('authenticate')  }}">
         @csrf
+        @if ($errors->any())
+        <p class="alert alert-danger">Die Zugangsdaten sind nicht korrekt!</p>
+        @endif
         <p><label><input class="form-control" type="text" name="name" placeholder="Benutzer"></label></p>
         <p><label><input class="form-control" type="password" name="password" placeholder="Passwort"></label></p>
         <p>

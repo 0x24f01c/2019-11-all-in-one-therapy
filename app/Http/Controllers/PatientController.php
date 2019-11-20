2c0b5fbb3f97ccd800f65a6fe159f4c7b98444e5
FS <gitea@fake.local>
2019-11-20 14:47:11 +0100

    added edit patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Patient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class PatientController extends Controller
 {
-    public function index(){
-        return view('patients', [
-            'patients'=>Patient::paginate(10)
+    public function index(Request $request)
+    {
+        if ($request->filled('search')) {
+            $suche = $request->post('search');
+            $patients = DB::table('patients')
+                ->where('lastname', 'like', '%' . $suche . '%')
+                ->orWhere('svnr', 'like', '%' . $suche . '%')
+                ->orWhere('email', 'like', '%' . $suche . '%')
+                ->paginate(10);
+            return view('patients', [
+                'patients' => $patients,
+            ]);
+        }
+        $patients = DB::table('patients')->paginate(10);
+        return view('patients.patients', [
+            'patients' => $patients,
         ]);
     }
+
+    public function edit($id)
+    {
+        $patients = DB::table('patients')->where('id', '=', $id)->get();
+        return view('patients.patientsEdit', [
+            'patients' => $patients,
+        ]);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
 }
+
